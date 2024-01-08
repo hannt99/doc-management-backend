3675f7ee93a33f917d6deb6fbee95858958f0415
Han Nguyen <hannt.kt@gmail.com>
2024-01-08 14:54:29 +0700

    update

--- src/controllers/auth-controllers.js
+++ src/controllers/auth-controllers.js
@@ -3,6 +3,7 @@ import jwt from 'jsonwebtoken';
 // import randomstring from 'randomstring';
 import bcrypt from 'bcryptjs';
 import sendMail from '../utils/email.js';
+import customLog from '../utils/customLog.js';
 
 // Generate verify email token
 // const generateVerifyEmailToken = (user, password) => {
@@ -53,7 +54,7 @@ const generateResetPasswordToken = (user) => {
         },
         process.env.RESET_PASS_SECRET,
         {
-            expiresIn: '600s',
+            expiresIn: '10800s',
         },
     );
 };
@@ -72,7 +73,7 @@ const generateResetPasswordToken = (user) => {
 //         const randomPass = randomstring.generate(7);
 //         const salt = bcrypt.genSaltSync(10);
 //         const hashPass = bcrypt.hashSync(randomPass, salt);
-//         
+//
 //         const newUser = new User({ ...req.body, password: hashPass });
 //         await newUser.save();
 
@@ -124,9 +125,9 @@ const generateResetPasswordToken = (user) => {
 // Sign in controller
 export const signInController = async (req, res) => {
     try {
-        // console.log("req: ", req);
+        customLog(req);
         const user = await User.findOne({ email: req.body.email }); // const user = await User.findOne({ userName: req.body.userName });
-        
+
         if (!user || user.isActived === false)
             return res.status(404).json({ code: 404, message: 'Tài khoản không tồn tại hoặc đã bị xóa' });
 
@@ -156,39 +157,159 @@ export const forgotPasswordController = async (req, res) => {
     try {
         const email = req.body.email;
 
-        // const isEmailExist = await User.findOne({ email: email });
-        // if (!isEmailExist) return res.status(403).json({ code: 403, message: 'Email không tồn tại' });
+        const isEmailExist = await User.findOne({ email: email });
+        if (!isEmailExist) return res.status(403).json({ code: 403, message: 'Email không tồn tại' });
 
-        // const userData = await User.findOne({ email: email });
-        // if (userData) {
-        //     const subject = 'Hệ thống quản lý văn bản - Đặt lại mật khẩu';
-        //     const resetPasswordToken = generateResetPasswordToken(userData);
-        //     const html = `<p> Xin chào ${userData.email}, Hãy nhấn vào <a href="${process.env.REACT_APP_BASE_URL}/reset-password">liên kết</a> này và đặt lại mật khẩu của bạn.</p>
-        //     <p>Thời gian hiệu lực trong vòng 10 phút.</p>`;
-        //     sendMail(userData.email, subject, html);
-        //     res.status(200).json({
-        //         code: 200,
-        //         message: 'Kiểm tra email và đặt lại mật khẩu của bạn',
-        //         resetPasswordToken: resetPasswordToken,
-        //     });
-        // } else {
-        //     res.status(400).json({ code: 400, message: 'Email không tồn tại' });
-        // }
-
-        let userData;
-        if (!(userData = await User.findOne({ email: email }))) {
-            return res.status(403).json({ code: 403, message: 'Email không tồn tại' });
-        } else {
+        const userData = await User.findOne({ email: email });
+        if (userData) {
             const subject = 'Hệ thống quản lý văn bản - Đặt lại mật khẩu';
             const resetPasswordToken = generateResetPasswordToken(userData);
-            const html = `<p> Xin chào ${userData.email}, Hãy nhấn vào <a href="${process.env.REACT_APP_BASE_URL}/reset-password">liên kết</a> này và đặt lại mật khẩu của bạn.</p>
-            <p>Thời gian hiệu lực trong vòng 10 phút.</p>`;
+            
+            // const html = `<p> Xin chào ${userData.email}, Hãy nhấn vào <a href="${process.env.REACT_APP_BASE_URL}/reset-password">liên kết</a> này và đặt lại mật khẩu của bạn.</p>
+            // <p>Thời gian hiệu lực trong vòng 10 phút.</p>`;
+            const html = `<div>
+            <div style="background-color:#fafafa; margin:0">
+                <table
+                    style="font-family:'Roboto', sans-serif, 'Open Sans'; font-size:14px; color:#5e5e5e; width:98%; max-width:600px; float:none; margin:0 auto"
+                    border="0" 
+                    cellpadding="0" 
+                    cellspacing="0" 
+                    valign="top" 
+                    align="left">
+                    <tbody>
+                        <tr align="middle">
+                            <td style="padding-top:30px; padding-bottom:32px"></td>
+                        </tr>
+                        <tr bgcolor="#ffffff">
+                            <td>
+                                <table 
+                                    bgcolor="#ffffff"
+                                    style="width:100%;line-height:20px;padding:32px;border:1px solid;border-color:#f0f0f0"
+                                    cellpadding="0">
+                                    <tbody>
+                                        <tr>
+                                            <td style="color:#5e5e5e; font-size:22px; line-height:22px">
+                                                Yêu cầu đặt lại mật khẩu
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td style="padding-top:24px; vertical-align:bottom">
+                                                Xin chào ${userData.fullName},
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td style="padding-top:24px">
+                                                Ai đó đã yêu cầu mật khẩu mới cho tài khoản QLVB của bạn 
+                                                <a 
+                                                    href="mailto:${userData.email}"
+                                                    target="_blank">
+                                                    ${userData.email}
+                                                </a>.
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td style="padding-top:24px">
+                                                <a 
+                                                    href=""
+                                                    style="color:#007dc1;text-decoration:none" 
+                                                    target="_blank">
+                                                    <span
+                                                        style="color:#007dc1;text-decoration:none"> 
+                                                        Liên hệ bộ phận hỗ trợ 
+                                                    </span>
+                                                </a> 
+                                                ngay lập tức nếu bạn không thực hiện yêu cầu này.
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td style="padding-top:24px">
+                                                Vui lòng nhấp vào nút bên dưới để hoàn tất quá trình.
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td align="center">
+                                                <table border="0" cellpadding="0" cellspacing="0" valign="top">
+                                                    <tbody>
+                                                        <tr>
+                                                            <td align="center"
+                                                                style="height:32px; padding-top:24px; padding-bottom:8px">
+                                                                <a
+                                                                    href="${process.env.REACT_APP_BASE_URL}/reset-password"
+                                                                    style="text-decoration:none" 
+                                                                    target="_blank"
+                                                                    data-saferedirecturl="">
+                                                                    <span style="padding:9px 32px 7px 31px; border:1px solid; text-align:center; color:#fff; border-radius:3px; background-color:#00bceb; border-color:#00bceb #00bceb #1e4471">
+                                                                        <span class="il">
+                                                                            Đặt lại
+                                                                        </span> 
+                                                                        mật khẩu 
+                                                                    </span> 
+                                                                </a>
+                                                            </td>
+                                                        </tr>
+                                                        <tr>
+                                                            <td align="center" style="color:#999">
+                                                                Hết hạn sau 3 giờ.
+                                                            </td>
+                                                        </tr>
+                                                    </tbody>
+                                                </table>
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td style="padding-top:24px">
+                                                Truy cập phần 
+                                                <a
+                                                    href=""
+                                                    style="color:#007dc1;text-decoration:none" 
+                                                    target="_blank"
+                                                    data-saferedirecturl="">
+                                                    <span style="color:#007dc1;text-decoration:none"> 
+                                                        trợ giúp 
+                                                    </span> 
+                                                </a> 
+                                                để biết thêm thông tin về đăng nhập, mật khẩu và thông tin tài khoản. 
+                                                <a 
+                                                    href=""
+                                                    style="color:#007dc1;text-decoration:none" 
+                                                    target="_blank"> 
+                                                    <span
+                                                        style="color:#007dc1;text-decoration:none">
+                                                        <br> Liên hệ bộ phận hỗ trợ
+                                                    </span>
+                                                </a> 
+                                                để được trợ giúp truy cập vào tài khoản của bạn.
+                                            </td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td style="font-size:12px; padding:16px 0 30px 50px; color:#999; text-align:center">
+                                Đây là tin nhắn được tạo tự động từ 
+                                <a 
+                                    href="" 
+                                    style="color:rgb(97,97,97)"
+                                    target="_blank"
+                                    data-saferedirecturl="">
+                                    QLVB
+                                </a>.
+                                Vui lòng không trả lời thư này.
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div></div>`;
+
             sendMail(userData.email, subject, html);
             res.status(200).json({
                 code: 200,
                 message: 'Kiểm tra email và đặt lại mật khẩu của bạn',
                 resetPasswordToken: resetPasswordToken,
             });
+        } else {
+            res.status(400).json({ code: 400, message: 'Email không tồn tại' });
         }
     } catch (error) {
         console.log(error);
